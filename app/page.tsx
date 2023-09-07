ded8db7220e2cd52a8d755cbdf7ad3ce53ba3765
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-07 08:21:02 +0300

    created the contact  page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import Blog from '@/components/Blog/Blog'
 import Carousel from '@/components/Carousel/Carousel'
 import Facilities from '@/components/Facilities/Facilities'
 import Gallery from '@/components/Gallery/Gallery'
+import Hero from '@/components/Hero/Hero'
+import ModalVideo from '@/components/ModalVideo/ModalVideo'
 import Rooms from '@/components/Rooms/Rooms'
 import Testimonial from '@/components/Testimonial/Testimonial'
 import { galleryData } from '@/data/galleryImages'
+import VideoThumb from '@/public/assets/images/modal-video-thumb.jpg'
 import { imageData } from '@/data/images'
-import Image from 'next/image'
+
 
 
 
@@ -17,13 +20,26 @@ export default function Home() {
 
     <main> 
 
+<Hero/>
 
-     <Carousel images={imageData}/>
+     {/* <Carousel images={imageData}/> */}
      <Facilities/>
      <Rooms/>
      <Testimonial/>
      <Gallery images={galleryData}/>
      <Blog/>
+{/* 
+
+
+                <ModalVideo
+  thumb={VideoThumb}
+  thumbWidth={768}
+  thumbHeight={432}
+  thumbAlt="Modal video thumbnail"
+  video="/assets/images/video.mp4"
+  videoWidth={1920}
+  videoHeight={1080} /> */}
+
 
     </main>
   )
